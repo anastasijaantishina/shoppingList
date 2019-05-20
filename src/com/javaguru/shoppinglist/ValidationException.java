48812ca86e3fb56718730c0373080f415b03c3c5
SergejKarpik <karpik2@inbox.lv>
2019-05-20 22:04:41 +0300

    Added name, price and discount Validation class version 2

--- src/com/javaguru/shoppinglist/ValidationException.java
+++ src/com/javaguru/shoppinglist/ValidationException.java
@@ -1,23 +1,8 @@
 package com.javaguru.shoppinglist;
 
- class ValidationException extends RuntimeException {
-
-    ValidationException() {
-    }
+class ValidationException extends RuntimeException {
 
     ValidationException(String message) {
         super(message);
-    }
-
-    ValidationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    ValidationException(Throwable cause) {
-        super(cause);
-    }
-
-    ValidationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
--- src/com/javaguru/shoppinglist/ValidationService.java
+++ src/com/javaguru/shoppinglist/ValidationService.java
@@ -4,84 +4,96 @@ import java.math.BigDecimal;
 
 public class ValidationService {
 
-    private BigDecimal minPrice = new BigDecimal(0.01).setScale(2, BigDecimal.ROUND_DOWN);      //минимальная цена товара
+    private final BigDecimal MIN_PRICE = new BigDecimal(0.01).setScale(2, BigDecimal.ROUND_DOWN);
 
-    private void spacesCountValidation(String name) {                                                       //метод проверяющий название на количество пробелов
-        char[] charArray = name.toCharArray();                                                              //преобразование строки в массив символов
-        int spacesCount = 0;                                                                                //количество пробелов
+    public void validateName(String name) {
+        nullNameValidation(name);
+        nameLengthValidation(name);
+        beginningAndEndOfNameValidation(name);
+        spacesCountValidation(name);
+        correctUseOfSpacesValidation(name);
+        useOfInvalidCharactersValidation(name);
+    }
 
-        for (char character : charArray) {                                                                  //цикл для прохода по всем ячейкам массива
-            if (character == ' ') {                                                                         //если символ является пробелом
-                spacesCount++;                                                                              //увеличить количество пробелов на 1
+    public void validatePrice(BigDecimal price) {
+        if ((price.compareTo(MIN_PRICE) < 0)) {
+            throw new ValidationException("Error! The price can not be less than " + MIN_PRICE + '$');
+        }
+    }
+
+    public void validateDiscount(int discount) {
+        if (discount > 80 || discount <= 0) {
+            throw new ValidationException("Error! Incorrect amount of discount.");
+        }
+    }
+
+    private void nullNameValidation(String name) {
+        if (name.isEmpty()) {
+            throw new ValidationException("Error! There is no name.");
+        }
+    }
+
+    private void nameLengthValidation(String name) {
+        if (name.length() < 3) {
+            throw new ValidationException("Error! Name is too short.");
+        }
+        if (name.length() > 30) {
+            throw new ValidationException("Error! Name is too long.");
+        }
+    }
+
+    private void beginningAndEndOfNameValidation(String name) {
+        if (name.startsWith(" ")) {
+            throw new ValidationException("Error! Name starts with a space.");
+        }
+        if (name.endsWith(" ")) {
+            throw new ValidationException("Error! Name ends with a space.");
+        }
+    }
+
+    private void spacesCountValidation(String name) {
+        char[] charArray = name.toCharArray();
+        int spacesCount = 0;
+
+        for (char character : charArray) {
+            if (character == ' ') {
+                spacesCount++;
             }
-            if (spacesCount > 3) {                                                                          //если количество пробелов больше заданого максимума
-                throw new ValidationException("Error! Name contains too much spaces.");                     //выбросить исключение
+            if (spacesCount > 3) {
+                throw new ValidationException("Error! Name contains too much spaces.");
             }
         }
     }
 
-    private void correctUseOfSpacesValidation(String name) {                                                //метод проверяющий не идут ли пробелы подряд
-        char[] charArray = name.toCharArray();                                                              //преобразование строки в массив символов
-        boolean previousSpace = false;                                                                      //предыдущий символ пробел
+    private void correctUseOfSpacesValidation(String name) {
+        char[] charArray = name.toCharArray();
+        boolean previousSpace = false;
 
-        for (char character : charArray) {                                                                  //цикл для прохода по всем ячейкам массива
-            if ((character == ' ') && (previousSpace)) {                                                    //если символ пробел и предыдущий символ был пробелом
-                throw new ValidationException("Error! Spaces in the name go one by one");                   //выбросить исключение
-            } else {                                                                                        //иначе
-                previousSpace = character == ' ';                                                           //предыдущий символ получает значение от логического выражения
+        for (char character : charArray) {
+            if ((character == ' ') && (previousSpace)) {
+                throw new ValidationException("Error! Spaces in the name go one by one");
+            } else {
+                previousSpace = character == ' ';
             }
         }
     }
 
-    private void useOfInvalidCharactersValidation(String name) {                                            //метод проверяющий название на запрещённые символы
-        char[] charArray = name.toCharArray();                                                              //преобразование строки в массив символов
+    private void useOfInvalidCharactersValidation(String name) {
+        char[] charArray = name.toCharArray();
 
-        for (char character : charArray) {                                                                  //цикл для прохода по всем ячейкам массива
-            if (character > 32 && character < 48) {                                                         //если номер символа соответствует запрещённому
-                throw new ValidationException("Error! Name contains invalid characters");                   //выбросить исключение
+        for (char character : charArray) {
+            if (character > 32 && character < 48) {
+                throw new ValidationException("Error! Name contains invalid characters");
             }
-            if (character > 57 && character < 65) {                                                         //если номер символа соответствует запрещённому
-                throw new ValidationException("Error! Name contains invalid characters");                   //выбросить исключение
+            if (character > 57 && character < 65) {
+                throw new ValidationException("Error! Name contains invalid characters");
             }
-            if (character > 90 && character < 97) {                                                         //если номер символа соответствует запрещённому
-                throw new ValidationException("Error! Name contains invalid characters");                   //выбросить исключение
+            if (character > 90 && character < 97) {
+                throw new ValidationException("Error! Name contains invalid characters");
             }
-            if (character > 122) {                                                                          //если номер символа соответствует запрещённому
-                throw new ValidationException("Error! Name contains invalid characters");                   //выбросить исключение
+            if (character > 122) {
+                throw new ValidationException("Error! Name contains invalid characters");
             }
-        }
-    }
-
-    public void validateName(String name) {                                                                 //проверка названия
-        if (name.isEmpty()) {                                                                               //если название пустое
-            throw new ValidationException("Error! There is no name.");                                      //выбросить исключение
-        }
-        if (name.length() < 3) {                                                                            //если длинна названия меньше минимальной
-            throw new ValidationException("Error! Name is too short.");                                     //выбросить исключение
-        }
-        if (name.length() > 30) {                                                                           //если длинна названия больше максимальной
-            throw new ValidationException("Error! Name is too long.");                                      //выбросить исключение
-        }
-        if (name.startsWith(" ")) {                                                                         //если название начинается с пробела
-            throw new ValidationException("Error! Name starts with a space.");                              //выбросить исключение
-        }
-        if (name.endsWith(" ")) {                                                                           //если название заканчивается пробелом
-            throw new ValidationException("Error! Name ends with a space.");                                //выбросить исключение
-        }
-        spacesCountValidation(name);                                                                        //проверка на количество пробелов в название
-        correctUseOfSpacesValidation(name);                                                                 //проверка на идущие подряд пробелы в название
-        useOfInvalidCharactersValidation(name);                                                             //проверка на запрещённые символы в название
-    }
-
-    public void validatePrice(BigDecimal price) {                                                           //проверка цены
-        if ((price.compareTo(minPrice) < 0)) {                                                              //если цена меньше минимальной
-            throw new ValidationException("Error! The price can not be less than " + minPrice + '$');       //выбросить исключение
-        }
-    }
-
-    public void validateDiscount(int discount) {                                                            //проверка скидки
-        if (discount > 80 || discount <= 0) {                                                               //если скидка  больше минимально или меньше 0
-            throw new ValidationException("Error! Incorrect amount of discount.");                          //выбросить исключение
         }
     }
 }
