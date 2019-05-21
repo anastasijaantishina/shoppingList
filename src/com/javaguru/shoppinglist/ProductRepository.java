bd691a89ad91056530018cfeb7e431c3f123955d
anastasiaantisina <nasta132@inbox.lv>
2019-05-21 20:10:21 +0300

    Delete and rename methods added. Mistakes fixed

---
+++ src/com/javaguru/shoppinglist/ProductRepository.java
@@ -0,0 +1,32 @@
+package com.javaguru.shoppinglist;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProductRepository implements Repository<Product> {
+
+    private Map<Long, Product> products = new HashMap<>();
+    private Long productIdSequence = Long.valueOf(0);
+
+    @Override
+    public void add(Product product) {
+        products.put(productIdSequence, product);
+        product.setId(productIdSequence);
+        productIdSequence++;
+    }
+
+    @Override
+    public void delete(Long id) {
+        products.remove(id);
+    }
+
+    public void changeProductName(Long id, String name) {
+        products.get(id).setName(name);
+    }
+
+    public void changeProductPrice(Long id, BigDecimal price) {
+        products.get(id).setPrice(price);
+    }
+
+}
