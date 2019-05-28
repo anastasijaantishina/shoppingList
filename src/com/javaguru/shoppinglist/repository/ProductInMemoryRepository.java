5e2faa37e3dcc86b3402525a498d3ee36bb38921
anastasiaantisina <nasta132@inbox.lv>
2019-05-28 22:23:52 +0300

    Database and product service packaging/arranging

--- src/com/javaguru/shoppinglist/repository/ProductInMemoryRepository.java
+++ src/com/javaguru/shoppinglist/repository/ProductInMemoryRepository.java
@@ -1,39 +1,46 @@
-package com.javaguru.shoppinglist;
+package com.javaguru.shoppinglist.repository;
+
+import com.javaguru.shoppinglist.Product;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ProductRepository implements Repository<Product> {
+public class ProductInMemoryRepository {
 
     private Map<Long, Product> products = new HashMap<>();
     private Long productIdSequence = Long.valueOf(0);
 
-    @Override
-    public void add(Product product) {
+    public Product put(Product product) {
         products.put(productIdSequence, product);
         product.setId(productIdSequence);
         productIdSequence++;
+        return product;
     }
 
-    @Override
-    public void delete(Long id) {
-        products.remove(id);
+    public Product delete(Long id) {
+        return products.remove(id);
     }
 
-    public void changeProductName(Long id, String name) {
+    public Product changeProductName(Long id, String name) {
         if (!products.containsKey(id)) {
             System.out.println("ID not found!");
         } else {
             products.get(id).setName(name);
         }
+        return products.get(id);
     }
 
-    public void changeProductPrice(Long id, BigDecimal price) {
+    public Product changeProductPrice(Long id, BigDecimal price) {
         if (!products.containsKey(id)) {
             System.out.println("ID not found!");
         } else {
             products.get(id).setPrice(price);
         }
+        return products.get(id);
+    }
+
+    public Product findById(Long id) {
+        return products.get(id);
     }
 }
--- src/com/javaguru/shoppinglist/service/ProductService.java
+++ src/com/javaguru/shoppinglist/service/ProductService.java
@@ -1,4 +1,35 @@
 package com.javaguru.shoppinglist.service;
 
+import com.javaguru.shoppinglist.Product;
+import com.javaguru.shoppinglist.repository.ProductInMemoryRepository;
+import com.javaguru.shoppinglist.service.validation.ProductValidationService;
+
+import java.math.BigDecimal;
+
 public class ProductService {
+
+    ProductInMemoryRepository repository = new ProductInMemoryRepository();
+    ProductValidationService validationService = new ProductValidationService();
+
+    public Long createProduct(Product product) {
+        validationService.validate(product);
+        Product createdProduct = repository.put(product);
+        return createdProduct.getId();
+    }
+
+    public Product findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public Product delete(Long id) {
+        return repository.delete(id);
+    }
+
+    public Product changeProductName(Long id, String name) {
+        return repository.changeProductName(id, name);
+    }
+
+    public Product changeProductPrice(Long id, BigDecimal price) {
+        return repository.changeProductPrice(id, price);
+    }
 }
