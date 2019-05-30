d97d9c77ac6268a8df4dd1fb6497fb593f0ddb1f
SergejKarpik <karpik2@inbox.lv>
2019-05-30 19:03:45 +0300

    Merge branch 'master' of C:\Users\User\IdeaProjects\shoppingList with conflicts.

---
+++ src/com/javaguru/shoppinglist/service/validation/ProductPriceValidationRule.java
@@ -0,0 +1,24 @@
+package com.javaguru.shoppinglist.service.validation;
+
+import com.javaguru.shoppinglist.Product;
+
+import java.math.BigDecimal;
+
+public class ProductPriceValidationRule implements ProductValidationRule {
+
+    private final BigDecimal MIN_PRICE = new BigDecimal(0.01).setScale(2, BigDecimal.ROUND_DOWN);
+
+    @Override
+    public void validate(Product product) {
+        if ((product.getPrice().compareTo(MIN_PRICE) < 0)) {
+            throw new ProductValidationException("Error! The price can not be less than " + MIN_PRICE + "Eur");
+        }
+    }
+
+    @Override
+    public void checkNotNull(Product product) {
+        if (product.getPrice() == null) {
+            throw new ProductValidationException("Product price must not be null");
+        }
+    }
+}
